dc4f9e05223883f9c10b6a1ffb7a5aefbdfce164
Jose Veras <jake0826@outlook.com>
2024-06-03 11:30:09 -0400

    Minor formatting Changes

--- 100 Days of Code/Beginner/Day2/tipcalculator.py
+++ 100 Days of Code/Beginner/Day2/tipcalculator.py
@@ -1,8 +1,8 @@
 print("Welcome to the tip calculator.")
-totalBill =  int(input("What was the total bill? "))
+totalBill = int(input("What was the total bill? "))
 peopleCount = int(input("How many people to split the bill? "))
 tipPercentage = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
 
-costPP = round(((totalBill / 100) * tipPercentage + totalBill) / peopleCount), 2
+costPP = round((((totalBill / 100) * tipPercentage + totalBill) / peopleCount), 2)
 
-print(f"Each person should pay: {costPP}")
+print("Each person should pay: {:.2f}".format(costPP))
--- 100 Days of Code/Beginner/Day3/lovecalculator.py
+++ 100 Days of Code/Beginner/Day3/lovecalculator.py
@@ -2,12 +2,14 @@ print("Welcome to the love calculator!")
 name1 = input("What is your name? \n").lower()
 name2 = input("What is their name? \n").lower()
 
-num1 = name1.count("t") + name1.count("r") + name1.count("u") + name1.count("e") + name2.count("t") + name2.count("r") + name2.count("u") + name2.count("e")
-num2 = name1.count("l") + name1.count("o") + name1.count("v") + name1.count("e") + name2.count("l") + name2.count("o") + name2.count("v") + name2.count("e")
-score = 
+num1 = (name1.count("t") + name1.count("r") + name1.count("u") + name1.count("e") + name2.count("t") +
+        name2.count("r") + name2.count("u") + name2.count("e"))
+num2 = (name1.count("l") + name1.count("o") + name1.count("v") + name1.count("e") + name2.count("l") +
+        name2.count("o") + name2.count("v") + name2.count("e"))
+score = int(str(num1) + str(num2))
 if score < 10 or score > 90:
     print(f"Your score is{num1}{num2}, you go together like coke and mentos.")
-elif score > 40 and score < 50:
+elif 40 < score < 50:
     print(f"Your score is {num1}{num2}, you are alright together.")
 else:
     print(f"Your score is {num1}{num2}.")
